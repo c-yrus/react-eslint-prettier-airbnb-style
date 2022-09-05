a432dbdf187ca08597441eae39b12255e1067c9f
c-yrus <Hazem0Abdennadher@gmail.com>
2022-09-05 11:35:49 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and see how eslint/prettier are working
+          together.
         </p>
         <a
           className="App-link"
